1ff5dc250e0b1171544a5f752fc8b9f2087e3d7a
bsh6308 <45328253+bsh6308@users.noreply.github.com>
2020-02-19 13:55:57 +0900

    Update saeamus-db.js

--- node-project/testNode/conf/saeamus-db.js
+++ node-project/testNode/conf/saeamus-db.js
@@ -4,23 +4,14 @@
 //
 var mysql = require("mysql");
 var dbconnection = mysql.createConnection({
-  host: "database-saeamus.c2m3gb1iap9w.ap-northeast-2.rds.amazonaws.com",
-  user: "saeamus",
-  password: "saeamus1111!",
+  host: "호스트주소",
+  user: "user name",
+  password: "암호",
   port: 3306,
-  database: "saeamus"
+  database: "db name"
 });
 
-////
-////테스트용 local DB
-////
-// var dbconnection = mysql.createConnection({
-//   host: "127.0.0.1",
-//   user: "root",
-//   password: "1234",
-//   port: 3306,
-//   database: "saeamus"
-// });
+
 
 ////
 ////테스트 커넥션
